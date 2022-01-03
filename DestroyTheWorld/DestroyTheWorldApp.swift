8b2d8d4b67181c0fec1d521dacad4b990bd47a85
Brigita Macaite <brimacaite@gmail.com>
2022-01-03 13:06:20 +0100

    Initial Commit

---
+++ DestroyTheWorld/DestroyTheWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  DestroyTheWorldApp.swift
+//  DestroyTheWorld
+//
+//  Created by Brigita Macaitė on 2022-01-03.
+//
+
+import SwiftUI
+
+@main
+struct DestroyTheWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
